5e5b53d1f30a6da1683c3a81d302f4915d5b888d
Yuriy Susuk <yuriy.susuk@scout24.com>
2018-11-20 20:12:04 +0100

    tranform and sink

--- src/main/scala/com/iuriisusuk/Main.scala
+++ src/main/scala/com/iuriisusuk/Main.scala
@@ -8,6 +8,7 @@ import fs2.{ Sink, Stream, StreamApp }
 import fs2.StreamApp.ExitCode
 import io.circe.Json
 import java.io.FileReader
+import java.nio.file.Paths
 import java.util.NoSuchElementException
 import java.util.concurrent.Executors
 import javax.xml.stream.events.Attribute
@@ -21,29 +22,54 @@ object Main extends StreamApp[IO] {
 
   override def stream(args: List[String], requestShutdown: IO[Unit]): Stream[IO, ExitCode] = {
 
+    case class See(name: String, lat: Option[String], long: Option[String], content: Option[String])
+    implicit val seeShow: Show[See] = Show { see => s"${see.name},${see.lat.getOrElse("None")},${see.long.getOrElse("None")},${see.content.getOrElse("None")}" }
+//    implicit val seeShorterShow: Show[See] = Show { see => s"${see.name},${see.lat.getOrElse("None")},${see.long.getOrElse("None")}" }
+    case class Do(name: String, lat: Option[String], long: Option[String], content: Option[String])
+
+    val blockingExecutionContext = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(2))
+
     val xmlInputFactory: XMLInputFactory = XMLInputFactory.newInstance
     xmlInputFactory.setProperty("javax.xml.stream.isCoalescing", true)
 
+    // todo: filter.map can be replaced with collect - for this to be doable, we need dsl which represents events (e.g. case class StartElement, case class EndElement)
+    // todo: {{do
+
+//    fs2.text.ut
     Stream.force {
-      IO(xmlInputFactory.createXMLEventReader(new FileReader(".//src//main//resources//enwikivoyage-20170620-pages-articles.xml"))).map { xmlStreamReader =>
-        Stream.repeatEval(IO(xmlStreamReader.nextEvent))
+      IO(xmlInputFactory.createXMLEventReader(new FileReader(".//src//main//resources//enwikivoyage-20170620-pages-articles.xml"))).map { xmlEventReader =>
+        Stream.repeatEval(IO(xmlEventReader.nextEvent))
           .filter(_.getEventType == XMLStreamConstants.START_ELEMENT)
           .map(_.asStartElement)
           .filter(_.getName.getLocalPart == "text")
-          .map(_ => Option(xmlStreamReader.peek))
+          .map(_ => Option(xmlEventReader.peek))
           .unNone
           .filter(_.getEventType == XMLStreamConstants.CHARACTERS)
           .map(_.asCharacters.getData)
           .filter(_.contains("{{see"))
           .flatMap { str =>
-            Stream.emits(StringUtils.substringsBetween(str, "{{see", "}}"))
+            (Stream.emits(StringUtils.substringsBetween(str, "{{see", "}}")).map { str =>
+              val m: Map[String, String] = str.split("\\|").filter(_.nonEmpty).map(_.trim).map { str =>
+                val fields: Array[String] = str.trim().split('=')
+                fields match {
+                  // case Array(key) => (key, None)
+                  case Array(key, value) => Some((key, value))
+                  case _ => None
+                }
+              }.flatten.toMap
+              m.get("name").map(name => See(name, m.get("lat"), m.get("long"), m.get("content")))
+            }.unNone.covary[IO])
           }
-          .take(10)
-      }
-    }
-      .observe(Sink.showLinesStdOut)
+      }}
+      // .take(5)
+      // .observe(Sink.showLinesStdOut[IO, See])
+      .map(_.show)
+      .intersperse("\n")
+      .through(fs2.text.utf8Encode)
+      .through(fs2.io.file.writeAll(Paths.get(".//pois.csv")))
+      .handleErrorWith( th => {println(th); Stream.empty})
+      .drain
       // handle/report errors properly
-      .handleErrorWith( _ => Stream.empty)
       .as(ExitCode.Success)
       .onFinalize(requestShutdown)
   }
