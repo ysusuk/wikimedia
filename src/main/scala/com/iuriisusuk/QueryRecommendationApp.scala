7a5291e8cafa7e179493e92145d66d66ac22ce08
Yuriy Susuk <yuriy.susuk@scout24.com>
2018-11-21 01:37:16 +0100

    app

---
+++ src/main/scala/com/iuriisusuk/QueryRecommendationApp.scala
@@ -0,0 +1,42 @@
+package com.iuriisusuk
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.fpm.FPGrowth
+
+import org.apache.spark.sql.functions._
+
+object QueryRecommendationApp {
+
+  def main(args: Array[String]) {
+
+    val spark = SparkSession
+      .builder
+      .appName("Ranking Application")
+      .getOrCreate
+
+    import spark.implicits._
+
+    val dataset = spark.read.format("csv")
+      .load("file:////Users/ysusuk/vidiq/wikimedia/pois.csv")
+      .toDF("name", "lat", "long", "content")
+
+    val dataset1 = dataset.withColumn("name", split(dataset("name"), " ").cast("array<string>"))
+
+    dataset1.show()
+
+    val fpgrowth = new FPGrowth().setItemsCol("name").setMinSupport(0.5).setMinConfidence(0.5)
+    val model = fpgrowth.fit(dataset1)
+
+    // Display frequent itemsets.
+    // model.freqItemsets.show()
+
+    // Display generated association rules.
+    // model.associationRules.show()
+
+    // transform examines the input items against all the association rules and summarize the
+    // consequents as prediction
+    model.transform(dataset1).show()
+
+    spark.stop
+  }
+}
